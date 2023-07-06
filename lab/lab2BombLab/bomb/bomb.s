e51c7ab72be742c5aba28eab8a9cd8b1f91d3766
jarzhang <1135470453@qq.com>
2023-07-06 13:16:14 +0800

    update phase1 in bomb.s

--- lab/lab2BombLab/bomb/bomb.s
+++ lab/lab2BombLab/bomb/bomb.s
@@ -345,11 +345,11 @@ Disassembly of section .text:
 
 0000000000400ee0 <phase_1>:
   400ee0:	48 83 ec 08          	sub    $0x8,%rsp
-  400ee4:	be 00 24 40 00       	mov    $0x402400,%esi
+  400ee4:	be 00 24 40 00       	mov    $0x402400,%esi ;将402400保存在esi
   400ee9:	e8 4a 04 00 00       	callq  401338 <strings_not_equal>
-  400eee:	85 c0                	test   %eax,%eax
-  400ef0:	74 05                	je     400ef7 <phase_1+0x17>
-  400ef2:	e8 43 05 00 00       	callq  40143a <explode_bomb>
+  400eee:	85 c0                	test   %eax,%eax;判断strings_not_equal结果是否为0
+  400ef0:	74 05                	je     400ef7 <phase_1+0x17>;为0则通过phase1
+  400ef2:	e8 43 05 00 00       	callq  40143a <explode_bomb>;不为0则爆炸
   400ef7:	48 83 c4 08          	add    $0x8,%rsp
   400efb:	c3                   	retq   
 
@@ -685,36 +685,37 @@ Disassembly of section .text:
   401311:	bf 08 00 00 00       	mov    $0x8,%edi
   401316:	e8 05 f9 ff ff       	callq  400c20 <exit@plt>
 
+;计算%rdi的长度,长度保存在%eax
 000000000040131b <string_length>:
-  40131b:	80 3f 00             	cmpb   $0x0,(%rdi)
+  40131b:	80 3f 00             	cmpb   $0x0,(%rdi) ;比较0和(%rdi)
   40131e:	74 12                	je     401332 <string_length+0x17>
-  401320:	48 89 fa             	mov    %rdi,%rdx
-  401323:	48 83 c2 01          	add    $0x1,%rdx
-  401327:	89 d0                	mov    %edx,%eax
+  401320:	48 89 fa             	mov    %rdi,%rdx;%rdi长度不是0，计算长度
+  401323:	48 83 c2 01          	add    $0x1,%rdx;%rdx表示当前指针指向的位置
+  401327:	89 d0                	mov    %edx,%eax;%eax表示偏移量
   401329:	29 f8                	sub    %edi,%eax
   40132b:	80 3a 00             	cmpb   $0x0,(%rdx)
   40132e:	75 f3                	jne    401323 <string_length+0x8>
   401330:	f3 c3                	repz retq 
-  401332:	b8 00 00 00 00       	mov    $0x0,%eax
+  401332:	b8 00 00 00 00       	mov    $0x0,%eax ;传入参数为0,return 0
   401337:	c3                   	retq   
-
+;比较%rid和%rsi是否相等,相等返回0,不相等返回1
 0000000000401338 <strings_not_equal>:
   401338:	41 54                	push   %r12
   40133a:	55                   	push   %rbp
-  40133b:	53                   	push   %rbx
-  40133c:	48 89 fb             	mov    %rdi,%rbx
+  40133b:	53                   	push   %rbx ;需要修改这三个register,临时保存到栈帧
+  40133c:	48 89 fb             	mov    %rdi,%rbx;计算第一个字符串参数的长度,长度保存在%r12d中
   40133f:	48 89 f5             	mov    %rsi,%rbp
   401342:	e8 d4 ff ff ff       	callq  40131b <string_length>
   401347:	41 89 c4             	mov    %eax,%r12d
-  40134a:	48 89 ef             	mov    %rbp,%rdi
+  40134a:	48 89 ef             	mov    %rbp,%rdi;计算第二个字符串参数的长度，长度保存在%eax中
   40134d:	e8 c9 ff ff ff       	callq  40131b <string_length>
   401352:	ba 01 00 00 00       	mov    $0x1,%edx
-  401357:	41 39 c4             	cmp    %eax,%r12d
-  40135a:	75 3f                	jne    40139b <strings_not_equal+0x63>
-  40135c:	0f b6 03             	movzbl (%rbx),%eax
-  40135f:	84 c0                	test   %al,%al
+  401357:	41 39 c4             	cmp    %eax,%r12d;比较两个字符串参数长度是否一样
+  40135a:	75 3f                	jne    40139b <strings_not_equal+0x63>;如果不一样则返回1结束
+  40135c:	0f b6 03             	movzbl (%rbx),%eax;将第一个参数第一位保存在%eax中
+  40135f:	84 c0                	test   %al,%al;%al就是%eax(八位的)
   401361:	74 25                	je     401388 <strings_not_equal+0x50>
-  401363:	3a 45 00             	cmp    0x0(%rbp),%al
+  401363:	3a 45 00             	cmp    0x0(%rbp),%al;循环比较两个参数的每一位,如果相等则返回0,不等则返回1
   401366:	74 0a                	je     401372 <strings_not_equal+0x3a>
   401368:	eb 25                	jmp    40138f <strings_not_equal+0x57>
   40136a:	3a 45 00             	cmp    0x0(%rbp),%al
